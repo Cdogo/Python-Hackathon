512fdf52eff83963ccc679759e9fd6a08669550f
Firesun4 <66979283+Firesun4@users.noreply.github.com>
2022-02-20 14:58:20 -0600

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ class ApiData:
     def getSolution(self):
         return self.apiData['queryresult'][ 'pods'][0]['subpods'][1]['plaintext']
 
+
+print(ApiData('x^2+3x+2').getAnswer())
 # eq = "3x+5%3D11"
 # webInfo = requests.get("http://api.wolframalpha.com/v2/query?appid=YGV9XJ-RH825P558W&input=solve+3x-7%3D11&podstate=Result__Step-by-step+solution&format=plaintext&includepodid=Result&output=json")
 # #print(webInfo.status_code) 200 if works 410 if not
